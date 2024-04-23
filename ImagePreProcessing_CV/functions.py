4642e7c57173b19f3d92dddd1f0d6d5c87fd5f73
TigistW <wondimnehtigist@gmail.com>
2024-04-23 12:05:40 +0300

    update function

--- ImagePreProcessing_CV/functions.py
+++ ImagePreProcessing_CV/functions.py
@@ -56,43 +56,43 @@ def apply_logarithmic_transformation(file_path, c):
 
     for x in range(width):
         for y in range(height):
-            if image.mode == 'RGB':  # If the mode is RGB
+            if image.mode == 'RGB':
                 r, g, b = image.getpixel((x, y))
-                # Apply logarithmic transformation to each channel
-                r_transformed = int(c * np.log1p(1 + r))  # Apply logarithmic transformation to the red channel
-                g_transformed = int(c * np.log1p(1 + g))  # Apply logarithmic transformation to the green channel
-                b_transformed = int(c * np.log1p(1 + b))  # Apply logarithmic transformation to the blue channel
+ 
+                r_transformed = int(c * np.log1p(1 + r)) 
+                g_transformed = int(c * np.log1p(1 + g))  
+                b_transformed = int(c * np.log1p(1 + b)) 
                 transformed_image.putpixel((x, y), (r_transformed, g_transformed, b_transformed))
-            elif image.mode == 'L':  # If the mode is grayscale
+                
+            elif image.mode == 'L': 
                 intensity = image.getpixel((x, y))
-                intensity_transformed = int(c * np.log1p(1 + intensity))  # Apply logarithmic transformation to the intensity
+                intensity_transformed = int(c * np.log1p(1 + intensity))
                 transformed_image.putpixel((x, y), intensity_transformed)
             else:
-                raise ValueError("Unsupported image mode: {}".format(image.mode))  # Raise error for unsupported image modes
+                raise ValueError("Unsupported image mode: {}".format(image.mode)) 
 
     return transformed_image
+
+
 def apply_contrast_stretching(file_path):
     image = Image.open(file_path)
     width, height = image.size
-    stretched_image = Image.new(image.mode, (width, height))  # Create a new image with the specified mode
+    stretched_image = Image.new(image.mode, (width, height))
 
     if image.mode == 'RGB':
         num_bands = 3 
     elif image.mode == 'L':
         num_bands = 1 
 
-    # Initialize min_val and max_val lists for each band
     min_val = [255] * num_bands 
     max_val = [0] * num_bands   
 
-    # Calculate minimum and maximum pixel values
     for x in range(width):
         for y in range(height):
             pixel_values = image.getpixel((x, y))
-            min_val = [min(v, p) for v, p in zip(min_val, pixel_values)]  # Update min values
-            max_val = [max(v, p) for v, p in zip(max_val, pixel_values)]  # Update max values
+            min_val = [min(v, p) for v, p in zip(min_val, pixel_values)] 
+            max_val = [max(v, p) for v, p in zip(max_val, pixel_values)] 
 
-    # Apply contrast stretching
     for x in range(width):
         for y in range(height):
             pixel_values = image.getpixel((x, y))
@@ -124,9 +124,7 @@ def apply_histogram_equalization(image_path):
 def apply_intensity_level_slicing(image_path, low_threshold, high_threshold, fill_color=(255, 255, 255)):
     # Read the image
     img = cv2.imread(image_path)
-
-    # Check if the image is in grayscale or RGBa format
-    if len(img.shape) == 2:  # Grayscale image
+    if len(img.shape) == 2:
   
         mask = cv2.inRange(img, low_threshold, high_threshold)
         img_sliced = np.where(mask != 0, img, fill_color)
